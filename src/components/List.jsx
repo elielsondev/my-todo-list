cc0a65e6ac71135caa9c7984b4791f824731e853
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-01-04 20:10:52 -0300

    style - List component styling

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,11 +1,11 @@
 export default function List () {
   return (
-    <section>
-      <div>
-        <p>Exemplo de anotação!</p>
-        <div>
+    <section className="flex justify-around flex-wrap p-4 m-2">
+      <div className="flex bg-white flex-col shadow-lg rounded-lg content-between justify-between hover:bg-gray-50 p-3 m-6 w-60 h-60">
+        <p className='text-wrap truncate'>Exemplo de anotação!</p>
+        <div className='flex justify-between items-center'>
           <p>04/01/2025 - 21:00h</p>
-          <button>Excluir</button>
+          <button className="bg-red-500 text-gray-50 p-1 rounded shadow-sm hover:bg-red-700 ">Excluir</button>
         </div>
       </div>
     </section>
