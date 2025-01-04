6e50b3977dcbd69c01d924ec23c00d1cb4a8dcc2
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-01-04 14:12:15 -0300

    feat - create page Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
+import Home from './pages/Home.jsx';
 import './App.css'
 
 export default function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Home />
     </>
   );
 };
