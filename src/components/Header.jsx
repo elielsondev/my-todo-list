9e92ae908285e01eb3d2b8c64825c60e28b298b7
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-01-04 17:28:47 -0300

    style - Header component styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header () {
   return (
-    <header>
-      <h1>My Todo List</h1>
+    <header className="flex justify-evenly items-center bg-sky-500 p-2 w-full shadow-lg	">
+      <h1 className="text-4xl font-sans text-sky-50 font-black">My Todo List</h1>
     </header>
   );
 };
