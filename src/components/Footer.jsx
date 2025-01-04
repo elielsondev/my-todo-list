9bd5024794b20567ceadc0b1753debd3aaac3e42
Elielson Nascimento <nascimento.elielson@gmail.com>
2025-01-04 18:00:30 -0300

    feat - create component Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+export default function Footer () {
+  return (
+   <footer>
+     <p>
+        <a
+          href="https://elielsondev.vercel.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Elielson Nascimento © {new Date().getFullYear()} - All rights reserved.
+        </a>
+      </p>
+   </footer> 
+  );
+};
